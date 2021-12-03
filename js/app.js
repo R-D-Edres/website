34c892c4619ba4217ae0a1c4e747e6e949dfd274
Abdullah AlOsaimi <osaimiacc@gmail.com>
2021-12-04 01:30:24 +0300

    the hamburger button kind of works now

--- js/app.js
+++ js/app.js
@@ -4,3 +4,15 @@ if ('serviceWorker' in navigator) {
     .then((reg) => console.log('service worker registered', reg))
     .catch((err) => console.log('service worker not registered', err));
 }
+
+
+const navSlide = () => {
+    const burger = document.querySelector('.hamburger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    })
+}
+
+navSlide();
